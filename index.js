48a54d7e13a06656f9e0cb3acfe9e6cbaace7efb
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-04-15 17:51:41 -0600

    refactored to go along with what we learned in class today

--- index.js
+++ index.js
@@ -1,77 +1,69 @@
-const inquirer = require("inquirer");
-var fs = require("fs");
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer")
 
-const questions = [
+const writeFileAsync = util.promisify(fs.writeFileSync);
 
-    {
-        type: "input",
-        message: "Title?",
-        name: "title"
-    },
-    {
-        type: "input",
-        message: "Description?",
-        name: "description"
-    },
-    {
-        type: "input",
-        message: "Table of Contents",
-        name: "toc"
-    },
-    {
-        type: "input",
-        message: "Installation",
-        name: "installation"
-    },
-    {
-        type: "input",
-        message: "Usage",
-        name: "usage"
-    },
-    {
-        type: "input",
-        message: "Licence",
-        name: "licence"
-    },
-    {
-        type: "input",
-        message: "Contributing",
-        name: "contributing"
-    },
-    {
-        type: "input",
-        message: "Tests",
-        name: "tests"
-    },
-    {
-        type: "input",
-        message: "Questions",
-        name: "questions"
-    }
-
-
-
-];
-
-function writeToFile(fileName, data) {
-    fs.appendFile(fileName, JSON.stringify(data, null, 4), function (error) {
-        if (error) {
-            return console.log(error);
-
-        }
-        else {
-
-            console.log(`successfully wrote to ${data.filename}`)
-        }
-    })
+async function promptUser() {
+    
+        return inquirer.prompt ([
+            {
+                type: "input",
+                message: "Title?",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "Description?",
+                name: "description"
+            },
+            {
+                type: "input",
+                message: "Installation",
+                name: "installation"
+            },
+            {
+                type: "input",
+                message: "Usage",
+                name: "usage"
+            },
+            {
+                type: "list",
+                message: "License",
+                name: "license",
+                choices: ["Apache","MIT","Mozilla"],
+                default: " "
+            },
+            {
+                type: "input",
+                message: "Contributors",
+                name: "contributors"
+            },
+            {
+                type: "input",
+                message: "Tests",
+                name: "tests"
+            },
+            {
+                type: "input",
+                message: "Questions",
+                name: "questions"
+            }            
+          ]);
+   
+   
 }
+function generateHTML(answers){};                                           
+promptUser()
+.then(function (answers) {
+    const formatAnswers = generateHTML(answers);
+   return writeFileAsync('index.html', answers)
+.then(function() {
+    console.log("Successfully wrote to index.html");
+  })
+.catch(function(err) {
+    console.log(err);
+  });
+});
 
-function init() {
-    inquirer.prompt(questions)
-        .then(function (response) {
-            writeToFile('readme.txt', response)
-        })
 
-}
-
-init();
